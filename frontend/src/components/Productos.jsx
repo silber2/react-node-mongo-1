bd2067f70ac2f06fd7c1c3abe38b2efee857ba6b
fsilber <fsilber@gmail.com>
2024-03-06 23:33:58 -0300

    modificando get productos.jsx

--- frontend/src/components/Productos.jsx
+++ frontend/src/components/Productos.jsx
@@ -12,11 +12,10 @@ export default function Productos() {
   useEffect(() => {
       axios.get("https://react-node-mongo-1.onrender.com/api/productos")
       .then(response => {
-        const resultado = response.data;
-        if (resultado.length <= 0) {
-          return console.error('its empty')
-        }
-        setProductos(resultado)
+        // if (response.data.length <= 0) {
+        //   return console.error('its empty')
+        // }
+        setProductos(response.data)
       })
       .catch(err => console.error(err + 'fetch error'))
   
