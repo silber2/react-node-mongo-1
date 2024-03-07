1e5cc7cb939c0a329aac1ef95dd331956bb81cd1
fsilber <fsilber@gmail.com>
2024-03-07 00:00:49 -0300

    mongo uri

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 
-mongoose.connect(process.env.DB_MONGO_URI)
+mongoose.connect("mongodb+srv://fransilber16:KMKtQn2qMyR1OU7w@cluster0.8nbblqs.mongodb.net/ecom?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {console.log('db connected')})
     .catch(err => console.error(err + 'db connection failed'))
