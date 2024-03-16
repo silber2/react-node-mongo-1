181ed061a2323788d00b89f9973d06b1b48d56e8
fsilber <fsilber@gmail.com>
2024-03-15 22:59:42 -0300

    key mp

--- frontend/src/components/mp.jsx
+++ frontend/src/components/mp.jsx
@@ -4,7 +4,7 @@ import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
 
 const YourComponent = ({preferenceID}) => {
     useEffect(() => {
-      initMercadoPago(import.meta.env.MP_PUBLIC_KEY, { locale: 'es-AR' });
+      initMercadoPago(import.meta.env.MP_PUBLIC_KEY || 'TEST-378f6ad9-5dfe-4379-bbfd-29c649e9e0a1', { locale: 'es-AR' });
     }, []);
 
     return (
